c3ca8e8b196c8467822a8016c5b2723253f88bc1
Dondou <106757087+dondou21@users.noreply.github.com>
2025-06-11 11:07:21 +0200

    Create 6 kyu Chain Evaluation.js

---
+++ 6 kyu Chain Evaluation.js
@@ -0,0 +1,19 @@
+Number.prototype.add = function(n) {
+  return this + n;
+};
+
+Number.prototype.subtract = function(n) {
+  return this - n;
+};
+
+Number.prototype.multiply = function(n) {
+  return this * n;
+};
+
+Number.prototype.divide = function(n) {
+  return this / n;
+};
+
+Number.prototype.square = function() {
+  return this * this;
+};
