16f3fcb3462781acc622acd420306da5a5374ad1
Dondou <106757087+dondou21@users.noreply.github.com>
2025-07-10 10:20:31 +0200

    Create 7 kyu Balance the parentheses.js

---
+++ 7 kyu Balance the parentheses.js
@@ -0,0 +1,22 @@
+function fixParentheses(str) {
+  let balance = 0;    
+  let open = 0; 
+
+  const array = str.split('');
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === '(') {
+      balance++;
+    } else if (array[i] === ')') {
+      if (balance === 0) {
+        
+        open++;
+      } else {
+        balance--;
+      }
+    }
+  }
+
+  
+  return '('.repeat(open) + str + ')'.repeat(balance);
+}
